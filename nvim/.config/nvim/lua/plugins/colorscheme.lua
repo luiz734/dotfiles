1ce9ec0803d00cac410c76d1a446103652232c54
Luiz G. <luiz007@live.com>
2024-03-08 11:19:55 -0300

    Add catppuccin-latte theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,17 @@
 return
 {
     {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function ()
+            require 'catppuccin'.setup({
+                flavour = "latte"
+            })
+            -- vim.cmd.colorscheme 'catppuccin'
+        end
+    },
+    {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
